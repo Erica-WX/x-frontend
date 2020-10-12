ac036804ff287a9ee838ce4f247d02cb7a16d4a7
wangxuan <2544194336@qq.com>
2020-10-12 13:10:27 +0800

    添加storybook addons

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,6 +1,5 @@
 import { configure } from '@storybook/react';
 
-
 function loadStories() {
   // automatically import all files ending in *.stories.ts
   // require.context('../stories', true, /.stories.tsx$/);
--- src/storybook/index.stories.tsx
+++ src/storybook/index.stories.tsx
@@ -1,15 +1,24 @@
 import '../theme/common.scss';
 import '../theme/index.scss';
 
+import { action } from '@storybook/addon-actions';
+import { withConsole } from '@storybook/addon-console';
+import { boolean, number, select, text, withKnobs } from '@storybook/addon-knobs';
+import { withViewport } from '@storybook/addon-viewport';
+import { addDecorator, storiesOf } from '@storybook/react';
 import React, { Component } from 'react';
-import { storiesOf } from '@storybook/react';
 import { Button } from '../components/common/button'; // 这里引入你想展示的组件
+
+addDecorator((storyFn, context) => withConsole()(storyFn)(context));
+addDecorator(withViewport());
+addDecorator(withKnobs);
+addDecorator((storyFn) => <div id="app">{storyFn()}</div>); //todo: add theme switcher
 
 storiesOf('Common Components', module)
   .add('Button', () =>   // 一个 add 表示添加一个 story
     <Button
       onClick={() => console.log('Button!') }
       size="small"
-      disabled
+      disabled={boolean('disabled', false)}
     >Button</Button>,
   );
